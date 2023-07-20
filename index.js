f3a397c8d2fa450f268beb34d9a6e4d2573425c2
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2023-07-20 16:38:22 +0900

    🔨Linter setup

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+const main = async () => {
+  console.log(process.env.JIRA_TOKEN);
+};
+
+main()
+  .then(() => {
+    console.log('The process is done');
+  })
+  .catch(() => {
+    console.log('the process failed');
+  });
